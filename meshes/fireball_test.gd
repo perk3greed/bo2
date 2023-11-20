9e24d0eca3940dbbb870a9b1d60a094bd6fc37b2
perk3greed <ab0055@yandex.ru>
2023-11-20 18:23:54 +0300

    some random comitt to check for bugs

--- meshes/fireball_test.gd
+++ meshes/fireball_test.gd
@@ -1,16 +1,32 @@
 extends Node3D
 
-
 var starting_pos 
 var player_pos
 var lifetime : int 
+var path_to_follow
+var path_calculated : bool = false
+var path_to_follow_normal
 
 func _physics_process(delta):
-	var path_to_follow = player_pos - starting_pos
-	var path_to_follow_normal = path_to_follow.normalized()
-
-	self.position += path_to_follow_normal*delta*15
+	if path_calculated == false:
+		path_to_follow = player_pos - starting_pos
+		path_to_follow_normal = path_to_follow.normalized()
+		path_calculated = true
+	
+	self.position += path_to_follow_normal*delta*4
 	lifetime += 1
 	if lifetime > 600:
 		print("fireball_died")
 		self.queue_free()
+
+
+
+func _on_area_3d_body_entered(body):
+	print(body)
+#
+#	if body.is_in_group("player"):
+#		print("player_hit")
+#		self.queue_free()
+#	elif body.is_in_group("wall") :
+#		print("something_else_hit")
+#		self.queue_free()
